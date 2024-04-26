1e668c6fbb4c8dd571e298551460d63a61ea2caf
Hunorabraham <hunor335@gmail.com>
2024-04-26 16:03:10 +0200

    Létrehoztam az Entity.cs fájlt, amely tartalmazza az Entity és az AABB osztályokat.

---
+++ Entity.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ugar
+{
+    public class AABB {
+        //position is centered, Size is half of the actual size
+        public Vector2 Position, Size;
+        public Vector2 ActualSize { get { return Size * 2; } }
+        //boolean for collision function to resolve overlaps automatically
+        public bool Resolve;
+        public AABB(Vector2 position, Vector2 size, bool resolve) { 
+            Position = position;
+            Size = size/2;
+            Resolve = resolve;
+        }
+        public bool TestPoint(Vector2 point)
+        {
+            if(Position.X-Size.X<point.X || Position.X+Size.X>point.X) return false;
+            if(Position.Y-Size.Y<point.Y || Position.Y+Size.Y>point.Y) return false;
+            return true;
+        }
+    }
+    public class Entity
+    {
+        //position is centered, Size is the Actual size
+        public Vector2 Position, Velocity, Size;
+        //it's collider
+        public AABB collider;
+        public Entity() {
+            
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -32,8 +32,9 @@ namespace Ugar
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) {
                 Exit();
+            }
 
             // TODO: Add your update logic here
 
