cd27d13b4d63723550c9c23679aacb9a83971305
Hunorabraham <hunor335@gmail.com>
2024-04-28 19:37:35 +0200

    added ScreenScale to Tool

--- Tool.cs
+++ Tool.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,5 +23,6 @@ namespace Ugar
             if (!CheckOverlap(a, b)) return;
             //add resolution function later.
         }
+        public static Vector2 ScreenScale = new Vector2(1920,1080);
     }
 }
