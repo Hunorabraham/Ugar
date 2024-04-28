fccff7533a3d8b5845e123db70941be35988e7ca
Hunorabraham <hunor335@gmail.com>
2024-04-28 19:19:12 +0200

    cleared up naming in the AABB class

--- Content/Tool.cs
+++ Content/Tool.cs
@@ -10,10 +10,10 @@ namespace Ugar.Content
     {
         public static bool CheckOverlap(AABB a, AABB b)
         {
-            if (a.Position.X - a.Size.X < b.Position.X + b.Size.X) return false;
-            if (b.Position.X - b.Size.X < a.Position.X + a.Size.X) return false;
-            if (a.Position.Y - a.Size.Y < b.Position.Y + b.Size.Y) return false;
-            if (b.Position.Y - b.Size.Y < a.Position.Y + a.Size.Y) return false;
+            if (a.Position.X - a.HalfSize.X < b.Position.X + b.HalfSize.X) return false;
+            if (b.Position.X - b.HalfSize.X < a.Position.X + a.HalfSize.X) return false;
+            if (a.Position.Y - a.HalfSize.Y < b.Position.Y + b.HalfSize.Y) return false;
+            if (b.Position.Y - b.HalfSize.Y < a.Position.Y + a.HalfSize.Y) return false;
             return true;
         }
         public static void Resolve(AABB a, AABB b)
