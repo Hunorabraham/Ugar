be814f72d8570bcff9dad48bc8cf1f11856afb5c
K-smith0 <122173ks@gmail.com>
2024-04-28 18:22:40 +0200

    Buttons

---
+++ Buttons.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+
+public class Button
+{
+	public Vector2 Position, Size;
+	public Func<int> OnClick;
+	public Button(Vector2 pos, Vector2 size, Func<int> onclick)
+	{  
+		Position = pos; 
+		Size = size; 
+		OnClick = onclick;
+	}
+	public Button(float x, float y, float width, float height, Func<int> onclick) 
+	{
+		Position = new Vector2(x, y);
+		Size = new Vector2(width, height);
+		OnClick = onclick;
+	}
+
+	public void DebugDraw()
+	{
+		
+	}
+}
