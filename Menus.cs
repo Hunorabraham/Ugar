764c01839c660929958647c28fc1d024ac78d5c0
K-smith0 <122173ks@gmail.com>
2024-05-02 17:38:07 +0200

    Character Select preampable

--- Menus.cs
+++ Menus.cs
@@ -41,9 +41,9 @@ namespace Ugar
             Game1.RenderList.Add(new Image(Game1.TextureList["MenuBackgroundLayer3"], new Vector2(0.5f, 0.5f), new Vector2(1.1f, 1.1f), 0));
             Game1.RenderList.Add(new Image(Game1.TextureList["MenuBackgroundLayer4"], new Vector2(0.5f, 0.5f), new Vector2(1.25f, 1.12f), 0));
             Game1.RenderList.Add(new Image(Game1.TextureList["Tablicsku"], new Vector2(0.85f, 0.7f), new Vector2(0.3f, 0.6f), 0));
-            Game1.ActiveButtons.Add(new Button(0.87f, 0.52f, 0.23f, 0.08f, Game1.TextureList["Play"], () => { return 0; }));
+            Game1.ActiveButtons.Add(new Button(0.87f, 0.52f, 0.23f, 0.08f, Game1.TextureList["Play"], () => { LoadCharacterSelect(T); return 0; }));
             Game1.ActiveButtons.Add(new Button(0.85f, 0.65f, 0.23f, 0.08f, Game1.TextureList["Logs"], () => { return 0; }));
-            Game1.ActiveButtons.Add(new Button(0.84f, 0.76f, 0.23f, 0.08f, Game1.TextureList["Settings"], () => { MenuManager.LoadSettings(T); return 0; }));
+            Game1.ActiveButtons.Add(new Button(0.84f, 0.76f, 0.23f, 0.08f, Game1.TextureList["Settings"], () => { LoadSettings(T); return 0; }));
             Game1.ActiveButtons.Add(new Button(0.86f, 0.88f, 0.23f, 0.08f, Game1.TextureList["QuitLabel"], () => { T.Exit(); return 0; }));
         }
         static public void LoadSettings(Game1 T)
@@ -59,6 +59,24 @@ namespace Ugar
             Game1.ActiveButtons.Add(new Button(0.24f, 0.40f, 0.23f, 0.08f, Game1.TextureList["HD+"], () => { Tool.TempScreenScale = new Vector2(1600, 900); return 0; }));
             Game1.ActiveButtons.Add(new Button(0.24f, 0.50f, 0.23f, 0.08f, Game1.TextureList["FullHD"], () => { Tool.TempScreenScale = new Vector2(1920, 1080); return 0; }));
             Game1.ActiveButtons.Add(new Button(0.77f, 0.77f, 0.23f, 0.08f, Game1.TextureList["Apply"], () => { Tool.ScreenScale = Tool.TempScreenScale; T._graphics.PreferredBackBufferWidth = (int)Tool.ScreenScale.X; T._graphics.PreferredBackBufferHeight = (int)Tool.ScreenScale.Y; T._graphics.ApplyChanges(); return 0; }));
+
+        }
+        static public void LoadCharacterSelect(Game1 T)
+        {
+            Game1.RenderList.Clear();
+            Game1.ActiveButtons.Clear();
+            Game1.PreviusHoveredButton = -1;
+            Game1.InMenu = false;
+            Game1.RenderList.Add(new Image(Game1.TextureList["SettingsBackground"], new Vector2(0.5f, 0.5f), new Vector2(1f, 1f), 0));
+            Game1.ActiveButtons.Add(new Button(0.24f, 0.78f, 0.23f, 0.08f, Game1.TextureList["Back"], () => { MenuManager.LoadMainMenu(T); return 0; }));
+            Game1.ActiveButtons.Add(new Button(0.20f, 0.39f, 0.18f, 0.42f, Game1.TextureList["DebugTexture0"], () => { return 0; }));
+            Game1.RenderList.Add(new Image(Game1.TextureList["DebugTexture0"], new Vector2(0.20f, 0.34f), new Vector2(0.16f, 0.29f), 0));
+            Game1.ActiveButtons.Add(new Button(0.39f, 0.39f, 0.18f, 0.42f, Game1.TextureList["DebugTexture0"], () => { return 0; }));
+            Game1.RenderList.Add(new Image(Game1.TextureList["DebugTexture0"], new Vector2(0.39f, 0.34f), new Vector2(0.16f, 0.29f), 0));
+            Game1.ActiveButtons.Add(new Button(0.58f, 0.39f, 0.18f, 0.42f, Game1.TextureList["DebugTexture0"], () => { return 0; }));
+            Game1.RenderList.Add(new Image(Game1.TextureList["DebugTexture0"], new Vector2(0.58f, 0.34f), new Vector2(0.16f, 0.29f), 0));
+            Game1.ActiveButtons.Add(new Button(0.77f, 0.39f, 0.18f, 0.42f, Game1.TextureList["DebugTexture0"], () => { return 0; }));
+            Game1.RenderList.Add(new Image(Game1.TextureList["DebugTexture0"], new Vector2(0.77f, 0.34f), new Vector2(0.16f, 0.29f), 0));
 
         }
     }
